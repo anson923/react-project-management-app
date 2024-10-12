725d1c9e66d7d46ea72a3d2957963f8aa8152e7f
Anson <ahikamiz0907@gmail.com>
2024-10-12 16:04:40 +0800

    added fallback home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import ProjectsSidebar from "./components/ProjectsSidebar.jsx";
 import NewProject from "./components/NewProject.jsx";
 import {useState} from "react";
+import DefaultHomePage from "./components/DefaultHomePage.jsx";
 
 function App() {
   const [newProjectVisible, setNewProjectVisible] = useState(false);
@@ -10,7 +11,7 @@ function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectsSidebar addProjectOnClick={() => handleProjectVisible(true)}/>
-      {newProjectVisible && <NewProject onCancelButtonClicked={() => handleProjectVisible(false)}/>}
+      {newProjectVisible ? <NewProject onCancelButtonClicked={() => handleProjectVisible(false)}/> : <DefaultHomePage addProjectOnClick={() => handleProjectVisible(true)}/>}
     </main>
 
   );
---
+++ src/components/DefaultHomePage.jsx
@@ -0,0 +1,18 @@
+import mainIcon from "../assets/no-projects.png";
+import Button from "./Button.jsx";
+export default function DefaultHomePage({addProjectOnClick}){
+  return (
+    <div className="text-center mt-24 w-2/3">
+      <img className="size-16 object-contain mx-auto" src={mainIcon} alt="homepageLogo"/>
+      <h2 className="text-xl font-bold text-stone-500 my-4">
+        No Project Selected.
+      </h2>
+      <p className="text-stone-400 mb-4">
+        Select a Project or get started with a new one.
+      </p>
+      <p className="mt-8">
+        <Button buttonOnClick={addProjectOnClick}>Create new project</Button>
+      </p>
+    </div>
+  );
+}
