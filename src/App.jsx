f934382fa0b2984eaac7ffcac0297a715ac8a999
Anson <ahikamiz0907@gmail.com>
2024-10-12 15:41:08 +0800

    added state to show or hide new Project page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import ProjectsSidebar from "./components/ProjectsSidebar.jsx";
 import NewProject from "./components/NewProject.jsx";
+import {useState} from "react";
 
 function App() {
+  const [newProjectVisible, setNewProjectVisible] = useState(false);
+  const handleProjectVisible = (isVisible) => {
+    setNewProjectVisible(isVisible);
+  }
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar/>
-      <NewProject/>
+      <ProjectsSidebar addProjectOnClick={() => handleProjectVisible(true)}/>
+      {newProjectVisible && <NewProject onCancelButtonClicked={() => handleProjectVisible(false)}/>}
     </main>
 
   );
